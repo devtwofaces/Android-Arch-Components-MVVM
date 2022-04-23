e732b06c828834d828e283ef1d3670ad8a36fe50
TwoFaces <dev.twofaces@gmail.com>
2022-04-23 19:24:29 +0530

    Initial Commit.

---
+++ app/src/main/java/com/twofaces/androidarchcomponents_mvvm/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.twofaces.androidarchcomponents_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
